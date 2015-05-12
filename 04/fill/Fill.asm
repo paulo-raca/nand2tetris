b4f2c0fe6816eafebfa227473ed5af3b1f5972c3
Paulo Costa <>
2015-05-11 22:11:25 -0300

    Week 4 - Multiply & Fill assembly programs

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -8,4 +8,54 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+//While (1) {
+(Loop)
+
+//	fillValue=0
+D=0
+@fillValue
+M=D
+
+//	if (kbd) {
+@KBD
+D=M
+@BeginFill
+D;JEQ
+
+//		fillValue=0xFFFF
+
+D=-1
+@fillValue
+M=D
+
+//	for (PTR=SCREEN; PTR<KBD; PTR++) {
+(BeginFill)
+//PTR=SCREEN
+@SCREEN
+D=A
+@PTR
+M=D
+
+(LoopFill)
+//	PTR<KBD
+@PTR
+D=M
+@KBD
+D=D-A
+@Loop
+D; JEQ
+
+//		*PTR=fillValue
+@fillValue
+D=M
+@PTR
+A=M
+M=D
+
+//		PTR++
+@PTR
+M=M+1
+
+//	} for (PTR=SCREEN; PTR<KBD; PTR++)
+@LoopFill
+0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,91 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//Pseudo-code:
+
+//A=R0
+//B=R1
+//mask=1
+//while (A) {
+//  if (mask & A) {
+//    ret += B
+//    A = A & ~mask
+//  }
+//  B *= 2
+//  mask *= 2
+//}
+//R2=ret
+
+//ret=0
+@ret
+M=0
+
+//A=R0
+@R0
+D=M
+@A
+M=D
+
+//B=R1
+@R1
+D=M
+@B
+M=D
+
+//mask=1
+@mask
+M=1
+
+//while (A) {
+(Loop)
+@A
+D=M
+@EndLoop
+D;JEQ
+
+//	if (mask & A) {
+@A
+D=M
+@mask
+D=M&D
+@skipSum
+D;JEQ
+
+//		ret += B
+@B
+D=M
+@ret
+M=M+D
+
+//		A = A & ~mask
+@mask
+D=!M
+@A
+M=M&D
+
+//	B *= 2
+(skipSum)
+@B
+D=M
+M=D+M
+
+
+//	mask *= 2
+@mask
+D=M
+M=D+M
+
+//} while (mask <= A) {
+@Loop
+0;JMP
+
+//R2 = ret
+(EndLoop)
+@ret
+D=M
+@R2
+M=D
+
+(End)
+@End
+0;JMP
